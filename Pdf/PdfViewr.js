e2e91ff613326532c24c546e6f22c38b9c9adba5
diego2000 <diego2000@gmail.com>
2020-10-03 10:39:38 -0600

    se refactorizo

--- Pdf/PdfViewr.js
+++ Pdf/PdfViewr.js
@@ -1,41 +1,162 @@
-import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
-import {source} from './consts/urls'
+import React, { useState, useEffect } from 'react';
+import {
+    StyleSheet,
+    TouchableHighlight,
+    Dimensions,
+    SafeAreaView,
+    View,
+    Text
+} from 'react-native';
+
 import Pdf from 'react-native-pdf';
+import Orientation from 'react-native-orientation-locker';
+import {source} from './consts/urls'
+const WIN_WIDTH = Dimensions.get('window').width;
+const WIN_HEIGHT = Dimensions.get('window').height;
+
+
+const PDF =() => {
+  const [page, setPage] = useState(1)
+  const [scale, setScale] = useState(1)
+  const [numberOfPages, setnumberOfPages] = useState(0)
+  const [horizontal, setHorizontal] = useState(false)
+  const [width, setWidth] = useState(WIN_WIDTH)
+
+  const  pdf = null;
+
+
+    const onOrientationDidChange = (orientation) => {
+        if (orientation == 'LANDSCAPE-LEFT'||orientation == 'LANDSCAPE-RIGHT') {
+          setWidth(WIN_HEIGHT>WIN_WIDTH?WIN_HEIGHT:WIN_WIDTH)
+          setHorizontal(true)
+
+        } else {
+            setWidth(WIN_HEIGHT>WIN_WIDTH?WIN_HEIGHT:WIN_WIDTH)
+          setHorizontal(false)
+        }
+    };
+    
+
+    useEffect(() => {
+       Orientation.addOrientationListener(onOrientationDidChange);
+      return () => {
+                Orientation.removeOrientationListener(onOrientationDidChange);
+      };
+    }, [])
+
+
+
+   const  prePage = () => {
+        let prePage = page > 1 ? page - 1 : 1;
+
+        pdf.setPage(prePage);
+        console.log(`prePage: ${prePage}`);
+    };
+
+    const nextPage = () => {
+        let nextPage = page + 1 > numberOfPages ? numberOfPages : page + 1;
+        pdf.setPage(nextPage);
+        console.log(`nextPage: ${nextPage}`);
+    };
+
+   const zoomOut = () => {
+        let scale = scale > 1 ? scale / 1.2 : 1;
+        setScale(sacale)
+        console.log(`zoomOut scale: ${scale}`);
+    };
+
+    const zoomIn = () => {
+        let scale = scale * 1.2;
+        scale = scale > 3 ? 3 : scale;
+        setScale(sacale)
+        console.log(`zoomIn scale: ${scale}`);
+    };
+
+    const switchHorizontal = () => {
+      setHorizontal(!horizontal)
+      setPage(page)
+    };
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={{flexDirection: 'row'}}>
+                    <TouchableHighlight disabled={page === 1}
+                                        style={page === 1 ? styles.btnDisable : styles.btn}
+                                        onPress={() => prePage()}>
+                        <Text style={styles.btnText}>{'-'}</Text>
+                    </TouchableHighlight>
+                    <View style={styles.btnText}><Text style={styles.btnText}>Page</Text></View>
+                    <TouchableHighlight disabled={page === numberOfPages}
+                                        style={page === numberOfPages ? styles.btnDisable : styles.btn}
+                                        onPress={() => nextPage()}>
+                        <Text style={styles.btnText}>{'+'}</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight disabled={scale === 1}
+                                        style={scale === 1 ? styles.btnDisable : styles.btn}
+                                        onPress={() => zoomOut()}>
+                        <Text style={styles.btnText}>{'-'}</Text>
+                    </TouchableHighlight>
+                    <View style={styles.btnText}><Text style={styles.btnText}>Scale</Text></View>
+                    <TouchableHighlight disabled={scale >= 3}
+                                        style={scale >= 3 ? styles.btnDisable : styles.btn}
+                                        onPress={() => zoomIn()}>
+                        <Text style={styles.btnText}>{'+'}</Text>
+                    </TouchableHighlight>
+                    <View style={styles.btnText}><Text style={styles.btnText}>{'Horizontal:'}</Text></View>
+                    <TouchableHighlight style={styles.btn} onPress={() => switchHorizontal()}>
+                        {!horizontal ? (<Text style={styles.btnText}>{'false'}</Text>) : (
+                            <Text style={styles.btnText}>{'true'}</Text>)}
+                    </TouchableHighlight>
+
+                </View>
+                <View style={{flex:1,width: width}}>
+                    <Pdf ref={(pdf) => {
+                        pdf = pdf;
+                    }}
+                         source={source}
+                         scale={scale}
+                         horizontal={horizontal}
+                         onLoadComplete={(numberOfPages, filePath,{width,height},tableContents) => {
+                           setnumberOfPages(numberOfPages)
+                          
+                             console.log(`total page count: ${numberOfPages}`);
+                             console.log(tableContents);
+                         }}
+                         onPageChanged={(page, numberOfPages) => {
+                           setPage(page)
+                             console.log(`current page: ${page}`);
+                         }}
+                         onError={(error) => {
+                             console.log(error);
+                         }}
+                         style={{flex:1}}
+                         />
+                </View>
+            </SafeAreaView>
+        )
+    }
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginTop: 25,
-  },
-  pdf: {
-    flex: 1,
-    width: 200,
-    height: 200,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 25,
+    },
+    btn: {
+        margin: 2,
+        padding: 2,
+        backgroundColor: "aqua",
+    },
+    btnDisable: {
+        margin: 2,
+        padding: 2,
+        backgroundColor: "gray",
+    },
+    btnText: {
+        margin: 2,
+        padding: 2,
+    }
 });
-const PDF = () => {
-  return (
-    <View style={styles.container}>
-      <Pdf
-        source={source}
-        onLoadComplete={(numberOfPages, filePath) => {
-          console.log(`number of pages: ${numberOfPages}`);
-        }}
-        onPageChanged={(page, numberOfPages) => {
-          console.log(`current page: ${page}`);
-        }}
-        onError={(error) => {
-          console.log(error);
-        }}
-        onPressLink={(uri) => {
-          console.log(`Link presse: ${uri}`);
-        }}
-        style={styles.pdf}
-      />
-    </View>
-  );
-};
 
 export default PDF;
--- Pdf/consts/urls.js
+++ Pdf/consts/urls.js
@@ -6,7 +6,11 @@ const source = {
 // local examples
 //const source = require('./test.pdf');  // ios only
  //const source = {uri:'bundle-assets://test.pdf'};
- 
+     //let source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
+        //let source = require('./test.pdf');  // ios only
+       //  let source = {uri:'bundle-assets://test.pdf'};
+
+        //let source = {uri:'file:///sdcard/test.pdf'};
 //const source = {uri:'file:///sdcard/test.pdf'};
 //const source = {uri:"data:application/pdf;base64,JVBERi0xLjcKJc..."};
 
